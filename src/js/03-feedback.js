e3ba3f0bea48172e84084cafdc0b90ec2bb02814
Yevhen Cherednyk <yevgen.cherednyk@gmail.com>
2022-10-18 23:18:28 +0300

    add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {};
+
+const refs = {
+  formRef: document.querySelector('.feedback-form'),
+  emailRef: document.querySelector('.feedback-form input'),
+  textareaRef: document.querySelector('.feedback-form textarea'),
+};
+
+refs.formRef.addEventListener('submit', onSubmit);
+refs.formRef.addEventListener('input', throttle(onFormInput, 500));
+
+populateInputForm();
+
+function onSubmit(evt) {
+  evt.preventDefault();
+
+  const {
+    elements: { email, message },
+  } = evt.currentTarget;
+  console.log({ email: email.value, message: message.value });
+
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  const message = JSON.stringify(formData);
+
+  localStorage.setItem(STORAGE_KEY, message);
+}
+
+function populateInputForm() {
+  const savedFormData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedFormData) {
+    refs.emailRef.value = savedFormData.email || '';
+    refs.textareaRef.value = savedFormData.message || '';
+  }
+}
